8ad98afd5cc235ad8667190c50bccd7638d06dde
fancyupsoft <apmsoft@gmail.com>
2022-05-25 11:49:16 +0900

    Update StringRandom.class.php
    __construct update

--- classes/string/StringRandom.class.php
+++ classes/string/StringRandom.class.php
@@ -19,7 +19,7 @@ class StringRandom {
 	protected $int_regs=array(0,1,2,3,4,5,6,7,8,9);
 
 	public function __construct(array $regs){
-		if(is_array($regs)){
+		if(is_array($regs) && count($regs)){
 			$this->regs = $regs;
 		}
 	}
