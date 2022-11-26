5c7f8b38e1b18fc0ea399613c9006971ce0de902
fancyupsoft <apmsoft@gmail.com>
2022-11-26 23:25:10 +0900

    util model d

--- classes/util/UtilModel.class.php
+++ classes/util/UtilModel.class.php
@@ -11,13 +11,13 @@ class UtilModel{
   }
   
   #@ return array
-  public function fetch(){
+  public function fetch() : array{
     return $this->arg;
   }
 
   #@ int
   # 배열갯수
-  public function length(){
+  public function length() : int{
     return (is_array($this->arg)) ? count($this->arg) : 0;
   }
 
@@ -30,7 +30,8 @@ class UtilModel{
       # 기존 값이 배열 일때
       if(is_array($this->arg[$propertyName])){
         if(is_array($value)){
-          if(count($this->arg[$propertyName]) && !isset($this->arg[$propertyName][0])){
+          if(count($this->arg[$propertyName]) && !isset($this->arg[$propertyName][0]))
+          {
             $pre_array = $this->arg[$propertyName];
             $this->arg[$propertyName] = array();
             $this->arg[$propertyName][] = $pre_array;
