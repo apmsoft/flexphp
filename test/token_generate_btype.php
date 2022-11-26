13c995647ff6ef18438560652020df921ff663b9
fancyupsoft <apmsoft@gmail.com>
2022-11-26 11:51:17 +0900

    token_generate_btype update

--- test/token_generate_btype.php
+++ test/token_generate_btype.php
@@ -3,10 +3,10 @@ $path = dirname(__DIR__);
 require $path. '/config/config.inc.php';
 
 # class
-# 지정문자로 생성
+# 지정문자로 생성 : 토큰 비교 가능
 # $tokenGenerateBtype = new \Flex\Token\TokenGenerateBtype('AE68A9MPVZ');
 
-# 랜덤 문자 생성
+# 랜덤 문자 생성 : 토큰 비교 불가
 $tokenGenerateBtype = new \Flex\Token\TokenGenerateBtype(null,10);
 
 # generateHashKey
