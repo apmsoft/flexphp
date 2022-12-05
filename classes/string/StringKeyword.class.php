af4480eaf060b9cc7153039ccb3d93a88583ae25
fancyupsoft <apmsoft@gmail.com>
2022-12-05 15:17:45 +0900

    string keyword update 및 예제 추가

--- classes/string/StringKeyword.class.php
+++ classes/string/StringKeyword.class.php
@@ -1,22 +1,28 @@
 <?php
 namespace Flex\String;
 
+# define : _CHRSET_
 # purpose : 문자을 변경하거나 더하거나 등 가공하는 역할을 한다.
 class StringKeyword
 {
 	private $keywords;
-	protected $filter_words = array
-	(
-		'할수'=>'','있습'=>'','니다'=>'','있나'=>'','있나요'=>'','하세요'=>'','되나'=>'','하는데'=>'','정해져'=>'','이루어'=>'','집니다'=>'',
-		'이제'=>'','만들'=>'','시켜야'=>'','언제나'=>'','그렇듯'=>'','그래'=>'','그리고'=>'','그러나'=>'','하지만'=>'','시키면'=>'','있는'=>'','처럼'=>'','시킬때'=>'','있다'=>'','정하다'=>'','정해진'=>'','습니다'=>'','보세요'=>''
-	);
-	protected $filter_end_words = array(
-		'가','이','은','는'
-	);
+	private $allow_tags = [];
 
-	public function __construct($keyword){
+	# 키워드 중 지우고 싶은 글자 및 단어
+	protected $filter_words = [];
+
+	# 키워드 중 끝 -1 글자 지우기
+	protected $filter_end_words = [];
+
+	public function __construct(string $keyword, array $allow_tags=[]){
 		if($keyword && $keyword !=''){
-			$this->keywords=self::cleanWord($keyword);
+			# 문자 특수 문자
+			if(is_array($allow_tags) && count($allow_tags)){
+				$this->allow_tags = $allow_tags;
+			}
+
+			# 특수 문자 제거
+			$this->keywords = self::cleanWord($keyword);
 		}
 	}
 
@@ -24,39 +30,64 @@ class StringKeyword
 	# return array()
 	private function cleanWord($keywords)
 	{
-		// $keywords = preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i",' ',$keywords);
-		// $keywords = preg_replace('/\s\s+/', ' ', $match[0]); // 연속된 공백을 하나로 제거
 		# 한글 영어 숫자만 추출
-		preg_match_all('/([\xEA-\xED][\x80-\xBF]{2}|[a-zA-Z0-9])+/', $keywords, $match);
+		$pattern = '/([\xEA-\xED][\x80-\xBF]{2}|[a-zA-Z0-9]';
+		if(count($this->allow_tags)){
+			# 허용된 특수 문자
+			$pattern .= '|[';
+			foreach($this->allow_tags as $etcstr){
+				$pattern .= '\\'.$etcstr;
+			}
+			$pattern .= ']';
+		}
+		$pattern .= ')+/';
+
+		# 클린
+		preg_match_all($pattern, $keywords, $match);
 		$keywords = $match[0];
 		if(is_array($keywords)){
 			foreach($keywords as $n => $w)
 			{
 				# 영어만 추출
 				preg_match_all("|(?<eng>[A-Za-z]+)|su", $w, $out);
-				$eng = (isset($out['eng'][0])) ?? '';
+				$eng = (isset($out['eng'][0])) ? $out['eng'][0] : '';
 				if($eng){
 					// echo $eng;
 					# 발견된 단어에서 영어만 삭제
-					$keywords[$n] = strtr($w, array($eng=>''));
+					$keywords[$n] = strtr($w, [$eng=>'']);
 					# 발견된 영단어 추가
 					$keywords[] = $eng;
 				}
 			}
-		}		
+		}
 
-		// $keywords = implode(' ',$argv);
-		// $keywords = preg_split("/[\s,]*\\\"([^\\\"]+)\\\"[\s,]*|" . "[\s,]*'([^']+)'[\s,]*|" . "[\s,]+/",$argv, 0, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
-		return array_unique($keywords);
+		return array_filter(array_unique($keywords));
 	}
 
 	# 분리된 단어중에서 필터 단어로 등록된 단어 지운 후 리턴
-	public function filterCleanWord(){
+	/**
+	 * filter_words : 틀정 문자를 특정문자로 변환
+	 * filter_end_words : 마지막 끝 글자를 특정문자로 변환
+	 */
+	public function filterCleanWord(array $filter_words =[], array $filter_end_words =[])
+	{
+		# init
+		if(is_array($filter_words) && count($filter_words)){
+			$this->filter_words = $filter_words;
+		}
+
+		if(is_array($filter_end_words) && count($filter_end_words)){
+			$this->filter_end_words = $filter_end_words;
+		}
+
+		# 필터
 		$argv = $this->keywords;
 		$data = array();
-		foreach($argv as $w){
+		foreach($argv as $w)
+		{
 			$s = strtr($w, $this->filter_words);
-			if($s && $s !=''){
+			if($s && $s !='')
+			{
 				$es = mb_substr($s, -1, NULL, _CHRSET_);
 				if (in_array($es, $this->filter_end_words)) {
 					$elen = mb_strlen($s, _CHRSET_) - 1;
---
+++ test/string_keyword.php
@@ -0,0 +1,37 @@
+<?php
+use Flex\App\App;
+use Flex\Log\Log;
+
+$path = dirname(__DIR__);
+require $path. '/config/config.inc.php';
+
+# 기본값 MESSAGE_FILE, log.txt;
+Log::init();
+
+# 화면에만 출력
+Log::init(Log::MESSAGE_ECHO);
+
+# keyword
+$query = '대한민국은!!^만만세 하세요&*korean ++/ 100%';
+$allow_tags = ['%','+'];
+
+# 키워드 허용된 특수 문자 제외하고 한글 | 영어 추출
+$stringKeyword = new \Flex\String\StringKeyword( $query, $allow_tags );
+$keywords = $stringKeyword->get_keywords();
+
+Log::d( $keywords );
+
+### 특정 글자 및 끝글자 삭제 하기 필터 기능 ######
+# 특정 글자 삭제
+$filter_words =['할수'=>'','있습'=>'','니다'=>'','있나'=>'','있나요'=>'','하세요'=>'','되나'=>'','하는데'=>'','정해져'=>'','이루어'=>'','집니다'=>'',
+'이제'=>'','만들'=>'','시켜야'=>'','언제나'=>'','그렇듯'=>'','그래'=>'','그리고'=>'','그러나'=>'','하지만'=>'','시키면'=>'','있는'=>'','처럼'=>'','시킬때'=>'','있다'=>'','정하다'=>'','정해진'=>'','습니다'=>'','보세요'=>''];
+
+# 끝 1글자 삭제
+$filter_end_words = [
+    '가','이','은','는'
+];
+$stringKeyword->filterCleanWord($filter_words, $filter_end_words);
+$keywords2 = $stringKeyword->get_keywords();
+
+Log::d( $keywords2 );
+?>
