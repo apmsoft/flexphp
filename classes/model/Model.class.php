4e7627d87ded309e1e7dc3ff934074094a7e9c6f
fancyupsoft <apmsoft@gmail.com>
2022-12-06 22:18:21 +0900

    Model class update

--- classes/model/Model.class.php
+++ classes/model/Model.class.php
@@ -19,32 +19,69 @@ class Model{
         return (is_array($this->args)) ? count($this->args) : 0;
     }
 
-    public function __set($propertyName, $value){
+    public function __set(string $propertyName, $value)
+    {
         # 키가 없을 경우
-        if(!array_key_exists($propertyName, $this->args)){
+        if(array_key_exists($propertyName, $this->args)){
             $this->args[$propertyName] = $value;
         }else{
             # 기존 값이 배열 일때
-            if(is_array($this->args[$propertyName]))
+            if(substr($propertyName,-1) == '+')
             {
-                if(is_array($value))
-                {
-                    if(count($value)){
-                        $pre_array = $this->args[$propertyName];
-                        $this->args[$propertyName] = [];
-                        $this->args[$propertyName] = $pre_array;
-                        $this->args[$propertyName][] = $value;
-                    }
+                $re_propertyname = strtr($propertyName,['+'=>'']);
+
+                # 값 선언이 배열이고 입력값이 string 일때
+                if(is_array($this->args[$re_propertyname])){
+                    $pre_array = $this->args[$re_propertyname];
+                    $this->args[$re_propertyname] = [];
+                    $this->args[$re_propertyname] = $pre_array;
                 }else{
-                    $this->args[$propertyName] = array_merge($this->args[$propertyName], (array)$value);
+                    $this->args[$re_propertyname] = [];
                 }
-            }else{
+                $this->args[$re_propertyname][] = $value;
+            }
+            else if(substr($propertyName,-1) == '-')
+            {
+                $re_propertyname = strtr($propertyName,['-'=>'']);
+
+                # 값 선언이 배열이고 입력값이 string 일때
+                if(is_array($this->args[$re_propertyname])){
+                    array_pop($this->args[$re_propertyname]);
+                }
+            }
+            else if(strpos($propertyName,'-{') !==false){
+                preg_match_all("/({+)(.*?)(})/", $propertyName, $matches);
+                $sno = strpos($propertyName,'-');
+                $re_propertyname = substr($propertyName,0,$sno);
+
+                # 값 선언이 배열이고 입력값이 string 일때
+                if(is_array($this->args[$re_propertyname])){
+                    $pre_array = $this->args[$re_propertyname];
+                    if(count($matches[2]) == 2){
+                        $this->args[$re_propertyname] = array_slice($pre_array, $matches[2][0],$matches[2][1]);
+                    }else{
+                        $this->args[$re_propertyname] = array_slice($pre_array, $matches[2][0]);
+                    }
+                }
+            }
+            else if(strpos($propertyName,'{') !==false){
+                preg_match_all("/({+)(.*?)(})/", $propertyName, $matches);
+                $sno = strpos($propertyName,'{');
+                $re_propertyname = substr($propertyName,0,$sno);
+
+                $pre_array =&$this->args[$re_propertyname];
+                foreach($matches[2] as $fieldName){
+                    $pre_array =&$pre_array[$fieldName];
+                }
+                $pre_array = $value;
+            }
+            else{
                 $this->args[$propertyName] = $value;
             }
         }
     }
 
-    public function __get($propertyName){
+    public function __get(string $propertyName){
         $result = null;
         if(array_key_exists($propertyName, $this->args)){
             $result = $this->args[$propertyName];
@@ -52,11 +89,11 @@ class Model{
     return $result;
     }
 
-    public function __isset($name){
+    public function __isset(string $name){
         return isset($this->args[$name]);
     }
 
-    public function __unset($name){
+    public function __unset(string $name){
         unset($this->args[$name]);
     }
 
--- test/model.php
+++ test/model.php
@@ -8,64 +8,117 @@ require $path. '/config/config.inc.php';
 # 기본값 MESSAGE_FILE, log.txt;
 Log::init();
 Log::init(Log::MESSAGE_ECHO);
+Log::options([
+    'datetime'   => false, # 날짜시간 출력여부
+    'debug_type' => true, # 디버그 타입 출력여부
+    'newline'    => true  # 개행문자 출력여부
+]);
 
 # 기본값 
-$args = [
-    'age' => 20
-];
 
 # 1
-$model = new \Flex\Model\Model( $args );
+$model = new \Flex\Model\Model( [] );
+$model->name = '홍길동';
+$model->age = 11;
 
 # model 데이터 전체 
 Log::d( $model->fetch() );
 
-# auto set key => value
-$model->name = '홍길동';
-
 # iseet
 if(isset($model->name)){
-    Log::d( 'model name  : true' );
+    Log::d( 'model name  : true' , $model->name);
 }
 
 # unset
 unset($model->name);
 if(!isset($model->namne)){
-    Log::d( 'model name  : unset' );
+    Log::d( 'model name  : unset');
 }
+Log::d("===============");
 
 # 배열 예제1 
 # 선언된 값이 배열일 경우 자동 순번 array_push /=============
 $model->list = [];
-$weeks = ['일','월','화','수','목','금','토'];
+$weeks = ['일','월','화'];
 foreach($weeks as $idx => $week_title)
 {
-    $model->list = [
+    $model->{"list+"} = [
         'no' => $idx,
         'title' => $week_title,
         'signdate' => date('Y-m-d H:i:s')
     ];
 }
 Log::d( $model->list );
+Log::d( $model->list[0]);
+Log::d("===============");
 
 # 배열 예제 2
-$model->pre_args = [
+$model->args = [
     'name' => '홍길동',
     'age' => 20
 ];
-Log::d( $model->pre_args );
+Log::d( $model->args );
+Log::d('name : ', $model->args['name']);
 
-# 자동으로 값을 추가
-$model->pre_args = ['name'=>'유관순'];
-$model->pre_args = ['name'=>'오솔이'];
-$model->pre_args = [['aaa'=>'bbbb']];
-Log::d( $model->pre_args );
+# args name키에 해당하는 값을 바꾼다
+Log::d('name :홍길동 -> name :  유관순으로 바꾸기');
+$model->{"args{name}"} = '유관순';
+
+Log::d($model->args);
+Log::d('name : ', $model->args['name']);
+Log::d("===============");
+
+
+
+# index array 로 되어 있는 값
+$model->loop = [];
+$model->{"loop+"} = ['no'=>1, 'title'=>'t1'];
+$model->{"loop+"} = ['no'=>2, 'title'=>'t2'];
+
+Log::d( $model->loop );
+
+# loop 문 index 에 해당하는 값 바꾸기
+Log::d('loop 문 index 에 해당하는 값 바꾸기');
+$model->{"loop{0}{title}"} = 'title1';
+
+Log::d( $model->loop );
+Log::d( $model->loop[0] );
+Log::d("===============");
+
+
+
+
 
 # 배열 예제3 스트링 배열 자동 증가
-$model->str_argv = [];
-$model->str_argv = '유관순';
-$model->str_argv = '홍길동';
-$model->str_argv = '이순신';
-$model->str_argv = '세종대왕';
-Log::d( $model->str_argv );
+$model->input = [];
+$model->{"input+"} = 'a';
+$model->{"input+"} = 'b';
+$model->{"input+"} = 'c';
+$model->{"input+"} = 'd';
+$model->{"input+"} = 'e';
+$model->{"input+"} = 'f';
+$model->{"input+"} = 'g';
+
+Log::d( $model->input );
+
+
+# 배열 뒤에서 하나 뺴기
+Log::d('배열 뒤에서 하나 뺴기 : input-');
+$model->{"input-"} = '';
+Log::d( $model->input );
+
+# 배열 index 번호 부터 끝까지
+Log::d('배열 index 번호 2부터 끝까지 : input-{2}');
+$model->{"input-{2}"} = '';
+Log::d( $model->input );
+
+# 뒤에서부터 3개만
+Log::d('뒤에서부터 -3개만 : input-{-3}');
+$model->{"input-{-3}"} = '';
+Log::d( $model->input );
+
+# 시작자릿수 - 갯수(몇개)
+Log::d('시작자릿수[0] - 갯수(2) : input-{0}{2}');
+$model->{"input-{0}{2}"} = '';
+Log::d( $model->input );
 ?>
