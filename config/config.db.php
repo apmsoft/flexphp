03ce49cfea38715619adb56bf95b7ca0e4e1d710
fancyupsoft <apmsoft@gmail.com>
2022-12-14 12:45:04 +0900

    ddd

--- config/config.db.php
+++ config/config.db.php
@@ -1,6 +1,6 @@
 <?php
 define('_DB_SHA2_ENCRYPT_KEY_','sfsfsfsafsafsfwarfdgjgejgesfsfsafksdfsfsvsvxzvasfddaaerdsadsgd');
-define('_DB_HOST_','localhost');
+define('_DB_HOST_','192.168.0.1');
 define('_DB_USER_','root');
 define('_DB_PASSWD_','d1004');
 define('_DB_NAME_','test_db');
