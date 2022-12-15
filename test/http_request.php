aeb1e613e62cb6c3df277c0f5887be4849c49194
fancyupsoft <apmsoft@gmail.com>
2022-12-15 23:20:37 +0900

    http request example update

--- test/http_request.php
+++ test/http_request.php
@@ -24,10 +24,37 @@ $urls[] = [
         // 'Authorization-Access-Token:safdsaesfdsafdsa'
     ]
 ];
+
+$urls[] = [
+    'url' => 'https://www.google.com/search?q=flexup&newwindow=1&rlz=1C5CHFA_enKR1016KR1016&sxsrf=ALiCzsYQE1MJoEP_h05RgJ1sFR-21K_JeQ%3A1671104725091&ei=1QibY7OdBdLZ-QaQzYKAAg&ved=0ahUKEwjz25Wcxvv7AhXSbN4KHZCmACAQ4dUDCA8&uact=5&oq=flexup&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIICAAQgAQQywEyCAgAEIAEEMsBMgoIABCABBAKEMsBMgoIABCABBAKEMsBMggIABCABBDLATIKCAAQgAQQChDLATIICAAQgAQQywEyCggAEIAEEAoQywEyCAgAEIAEEMsBMg4ILhCABBDHARCvARDLAToKCAAQRxDWBBCwAzoECCMQJzoECAAQQzoNCC4QxwEQ0QMQ1AIQQzoHCCMQ6gIQJzoNCC4QxwEQ0QMQ6gIQJzoLCAAQgAQQsQMQgwE6BQgAEIAEOggIABCABBCxAzoNCAAQgAQQsQMQgwEQCkoECEEYAEoECEYYAFCeBljMIGCxI2gEcAF4AIABfYgBqAaSAQMwLjeYAQCgAQGwAQrIAQrAAQE&sclient=gws-wiz-serp',
+    'params' => [
+        // 'id' => 'test@dd.com',
+        // 'msg'=> 'ok'
+    ],
+    'headers'=> [
+        // 'Authorization-Access-Token:safdsaesfdsafdsa'
+    ]
+];
 try{
     $httpRequest = new Flex\Http\HttpRequest();
-    $httpRequest($urls)->get(function($data){
-        Log::d($data);
+    $httpRequest($urls)->get(function($data) use ($urls){
+        if(is_array($data)){
+            foreach($data as $idx => $contents){
+                Log::d('-------------------------');
+                Log::d('-------------------------');
+                Log::d('-------------------------',$urls[$idx]['url']);
+                Log::d('-------------------------');
+                Log::d('-------------------------');
+
+                $htmlXssChars = new \Flex\Html\HtmlXssChars( $contents );
+                Log::d($htmlXssChars->getContext('XSS'));
+                Log::d('-------------------------');
+                Log::d('-------------------------');
+                Log::d('-------------------------');
+                Log::d('-------------------------');
+                Log::d('-------------------------');
+            }
+        }
         #return $data[0];
     });
     // $httpRequest($urls)->post(function($data){
