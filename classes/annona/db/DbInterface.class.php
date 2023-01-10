3a31fd3f317e5d8e60742d4d326b7e14bf0b6529
fancyupsoft <apmsoft@gmail.com>
2023-01-10 14:59:10 +0900

    DbInterface update

--- classes/annona/db/DbInterface.class.php
+++ classes/annona/db/DbInterface.class.php
@@ -4,7 +4,7 @@ namespace Flex\Annona\Db;
 # purpose : 각종 SQL 관련 디비를 통일성있게  작성할 수 있도록 틀을 제공
 interface DbInterface
 {
-    public function query(string $query, int $result_mode) : mixed;			# 쿼리
+    public function query(string $query, mixed $result_mode) : mixed;			# 쿼리
     public function insert() : bool;			# 저장
     public function update() : bool;	# 수정
     public function delete() : bool;	# 삭제
