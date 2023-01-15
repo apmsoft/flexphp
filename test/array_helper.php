5b884a97ac24cef30d7cbb2453a7c37b157238f4
fancyupsoft <apmsoft@gmail.com>
2023-01-15 17:04:41 +0900

    example array_helper

--- test/array_helper.php
+++ test/array_helper.php
@@ -76,93 +76,93 @@ foreach($arrays_desc as $desc)
 // Log::d ("=================================");
 
 # 첫번째로 발견된 배열 받기
-$find_first_args = (new ArrayHelper( $args ))->sorting('lowPrice','DESC')->find("productName","z")->value;
+$find_first_args = (new ArrayHelper( $args ))->sorting('lowPrice','DESC')->find("productName","A")->value;
 Log::d('find_first_args ', $find_first_args);
 
 # 첫번째로 발견된 배열 index 키값 돌려받기
-// $find_index = (new ArrayHelper( $args ))->sorting('lowPrice','DESC')->findIndex("muid",2);
-// Log::d('find_index ', $find_index);
+$find_index = (new ArrayHelper( $args ))->sorting('lowPrice','DESC')->findIndex("muid",2);
+Log::d('find_index ', $find_index);
 
-// # 검색에 해당하는 전체 배열 받기 => (key, value)
-// $find_all = (new ArrayHelper( $args ))->findAll("productName","A", "B")->value;
-// Log::d($find_all);
+# 검색에 해당하는 전체 배열 받기 => (key, value)
+$find_all = (new ArrayHelper( $args ))->findAll("productName","A", "B")->value;
+Log::d($find_all);
 
-// # 특정키에 해당하는 여러가지 값에 해당하는 배열값 찾기 - 방법 1 => (key, values)
-// // $find_all = (new ArrayHelper( $args ))->findAll("productName",["I","G"])->value;
-// // Log::d("find all 멀티 값 방법 1",$find_all);
+# 특정키에 해당하는 여러가지 값에 해당하는 배열값 찾기 - 방법 1 => (key, values)
+// $find_all = (new ArrayHelper( $args ))->findAll("productName",["I","G"])->value;
+// Log::d("find all 멀티 값 방법 1",$find_all);
 
-// # 특정키에 해당하는 여러가지 값에 해당하는 배열값 찾기 - 방법 2  => (key, values)
-// // $find_all = (new ArrayHelper( $args ))->findAll("productName","I","G")->value;
-// // Log::d("find all 멀티 값 방법 2", $find_all);
+# 특정키에 해당하는 여러가지 값에 해당하는 배열값 찾기 - 방법 2  => (key, values)
+// $find_all = (new ArrayHelper( $args ))->findAll("productName","I","G")->value;
+// Log::d("find all 멀티 값 방법 2", $find_all);
 
-// # 멀티 키=>밸류에 해당하는 값 배열 모두 찾기 AND
-// $find_where_all = (new ArrayHelper( $args ))->findWhere(["productName"=>'G',"lowPrice"=>27200])->value;
-// Log::d("find where AND", $find_where_all);
+# 멀티 키=>밸류에 해당하는 값 배열 모두 찾기 AND
+$find_where_all = (new ArrayHelper( $args ))->findWhere(["productName"=>'G',"lowPrice"=>27200])->value;
+Log::d("find where AND", $find_where_all);
 
-// # 멀티 키=>밸류에 해당하는 값 배열 모두 찾기 OR
-// $find_where_all = (new ArrayHelper( $args ))->findWhere(["productName"=>'G',"lowPrice"=>27200], 'OR')->value;
-// Log::d("find where OR", $find_where_all);
+# 멀티 키=>밸류에 해당하는 값 배열 모두 찾기 OR
+$find_where_all = (new ArrayHelper( $args ))->findWhere(["productName"=>'G',"lowPrice"=>27200], 'OR')->value;
+Log::d("find where OR", $find_where_all);
 
-// # 특정키를 중심으로 중복 데이터 제거
-// $unique_arg = (new ArrayHelper( $args ))->unique("muid")->value;
-// Log::d("unique_arg ", $unique_arg);
+# 특정키를 중심으로 중복 데이터 제거
+$unique_arg = (new ArrayHelper( $args ))->unique("muid")->value;
+Log::d("unique_arg ", $unique_arg);
 
-// # 멀티키밸 찾기와 중복 제거 findWhere -> unique
-// // $findwhere_unique_arg = (new ArrayHelper( $args ))->findWhere(["productName"=>'G',"lowPrice"=>27200],'OR')->value;
-// // Log::d("findwhere_unique_arg ", $findwhere_unique_arg);
-
-// // Log::d ("=================================");
-
-// # 배열 끝에 추가
-// $append_args = [
-// 	"rank"=> 10,
-// 	"lowPrice"=> 22000,
-// 	"productName"=> "J",
-// ];
-
-// $append = (new ArrayHelper( $args ))->append( $append_args )->value;
-// Log::d('append ', $append);
-
-
-// # 배열 끝에 추가 후 ---> 배열 정렬 ASC
-// $append_and_sorting_asc = (new ArrayHelper( $args ))->append($append_args)->sorting('lowPrice','ASC')->value;
-// Log::d('append_and_sorting_asc ', $append_and_sorting_asc);
+# 멀티키밸 찾기와 중복 제거 findWhere -> unique
+// $findwhere_unique_arg = (new ArrayHelper( $args ))->findWhere(["productName"=>'G',"lowPrice"=>27200],'OR')->value;
+// Log::d("findwhere_unique_arg ", $findwhere_unique_arg);
 
 // Log::d ("=================================");
 
-// # 값이 [숫자]로 되어 있는 키의 총합을 구한다 (SUM)
-// $numeric_sum = (new ArrayHelper( $args ))->sum('lowPrice');
-// Log::d('numeric_sum ', '총합  ', $numeric_sum);
+# 배열 끝에 추가
+$append_args = [
+	"rank"=> 10,
+	"lowPrice"=> 22000,
+	"productName"=> "J",
+];
 
-// # 값이 [숫자]로 되어 있는 키의 평균을 구한다 (AVG)
-// $numeric_avg = (new ArrayHelper( $args ))->avg('lowPrice');
-// Log::d('numeric_avg ', '평균값', $numeric_avg);
+$append = (new ArrayHelper( $args ))->append( $append_args )->value;
+Log::d('append ', $append);
 
-// # union
-// $a_args = [
-// 	["id"=>1,"name"=>"A","signdate"=>"20121100"],
-// 	["id"=>2,"name"=>"B","signdate"=>"20121100"]
-// ];
-// $b_args = [
-// 	["id"=>1,"email"=>"aa@gmail.com","signdate"=>"20121200"],
-// 	["id"=>2,"email"=>"bb@gmail.com","signdate"=>"20121200"]
-// ];
-// $union = (new ArrayHelper( ["a"=>$a_args,"b"=>$b_args] ))
-// ->union(["a"=>"id,name","b"=>"email"])
-// ->value;
-// Log::d("union", $union);
 
-// # union findWhere
-// // $union_findall = (new ArrayHelper( ["b"=>$args2,"a"=>$args,"c"=>$args3] ))
-// // ->union(["a"=>"muid,lowPrice,productName","b"=>"name,userid","c"=>"title"])->findAll("muid",1)->sorting('lowPrice','DESC')->value;
-// // Log::d("union_findall", $union_findall);
+# 배열 끝에 추가 후 ---> 배열 정렬 ASC
+$append_and_sorting_asc = (new ArrayHelper( $args ))->append($append_args)->sorting('lowPrice','ASC')->value;
+Log::d('append_and_sorting_asc ', $append_and_sorting_asc);
 
-// # 멀티 키=>값으로 배열 index 키 찾기
-// $args = json_decode('[{"muid":"385","dvmac":"5CF2864123A7","module_id":"comkwatercj"},{"muid":"27","dvmac":"5CF2864123A7"},{"muid":"226","dvmac":"5CF28643850B"},{"muid":"27","dvmac":"5CF2864123E5","module_id":"comkwatercj"},{"muid":"25","dvmac":"5CF2864123E5"}]',true);
-// Log::d($args);
+Log::d ("=================================");
 
-// $find_where_index = (new ArrayHelper( $args ))->findWhereIndex(["muid"=>27,"dvmac"=>'5CF2864123A7']);
-// Log::d($find_where_index);
-// $find_where_index = (new ArrayHelper( $args ))->findWhereIndex(["muid"=>27,"dvmac"=>'5CF2864123E5']);
-// Log::d($find_where_index);
+# 값이 [숫자]로 되어 있는 키의 총합을 구한다 (SUM)
+$numeric_sum = (new ArrayHelper( $args ))->sum('lowPrice');
+Log::d('numeric_sum ', '총합  ', $numeric_sum);
+
+# 값이 [숫자]로 되어 있는 키의 평균을 구한다 (AVG)
+$numeric_avg = (new ArrayHelper( $args ))->avg('lowPrice');
+Log::d('numeric_avg ', '평균값', $numeric_avg);
+
+# union
+$a_args = [
+	["id"=>1,"name"=>"A","signdate"=>"20121100"],
+	["id"=>2,"name"=>"B","signdate"=>"20121100"]
+];
+$b_args = [
+	["id"=>1,"email"=>"aa@gmail.com","signdate"=>"20121200"],
+	["id"=>2,"email"=>"bb@gmail.com","signdate"=>"20121200"]
+];
+$union = (new ArrayHelper( ["a"=>$a_args,"b"=>$b_args] ))
+->union(["a"=>"id,name","b"=>"email"])
+->value;
+Log::d("union", $union);
+
+# union findWhere
+// $union_findall = (new ArrayHelper( ["b"=>$args2,"a"=>$args,"c"=>$args3] ))
+// ->union(["a"=>"muid,lowPrice,productName","b"=>"name,userid","c"=>"title"])->findAll("muid",1)->sorting('lowPrice','DESC')->value;
+// Log::d("union_findall", $union_findall);
+
+# 멀티 키=>값으로 배열 index 키 찾기
+$args = json_decode('[{"muid":"385","dvmac":"5CF2864123A7","module_id":"comkwatercj"},{"muid":"27","dvmac":"5CF2864123A7"},{"muid":"226","dvmac":"5CF28643850B"},{"muid":"27","dvmac":"5CF2864123E5","module_id":"comkwatercj"},{"muid":"25","dvmac":"5CF2864123E5"}]',true);
+Log::d($args);
+
+$find_where_index = (new ArrayHelper( $args ))->findWhereIndex(["muid"=>27,"dvmac"=>'5CF2864123A7']);
+Log::d($find_where_index);
+$find_where_index = (new ArrayHelper( $args ))->findWhereIndex(["muid"=>27,"dvmac"=>'5CF2864123E5']);
+Log::d($find_where_index);
 ?>
