a47f6d9ee8a2b898a687d60cff3be5cca16a68bf
fancyupsoft <apmsoft@gmail.com>
2023-01-17 12:58:29 +0900

    _fn function php update

--- function/_fn.function.php
+++ function/_fn.function.php
@@ -1,3 +1,12 @@
 <?php
-# 사용 함수를 넣는 파일
+function sns_time_format(array $snsf, string $time){
+	return match($snsf[1]) {
+		'second','seconds' => sprintf("%d초전",$snsf[0]),
+		'minute','minutes' => sprintf("약%d분전",$snsf[0]),
+		'hour','hours'     => sprintf("약%d시간전",$snsf[0]),
+		'day','days'       => sprintf("약%d일전",$snsf[0]),
+		// 'month','months'   => sprintf("약%d 개월전",$snsf[0]),
+		default            => $time ?? ''
+	};
+}
 ?>
