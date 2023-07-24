7a9ee0353b6d1675be20ccf5ed0fbf4f444f08cd
fancyupsoft <apmsoft@gmail.com>
2023-07-24 10:57:52 +0900

    schema type update

---
+++ test/components/schema_enum.php
@@ -0,0 +1,37 @@
+<?php
+use Flex\Annona\App;
+use Flex\Annona\Log;
+use Flex\Annona\R;
+
+// use Flex\Components\Columns\ColumnsEnum;
+use Flex\Components\Schema\SchemaEnum;
+use Flex\Annona\Array\ArrayHelper;
+
+$path = dirname(dirname(__DIR__));
+require $path. '/config/config.inc.php';
+
+# 기본값 MESSAGE_FILE, log.txt;
+Log::init();
+Log::init(Log::MESSAGE_ECHO);
+
+Log::options([
+    'datetime'   => false, # 날짜시간 출력여부
+    'debug_type' => true, # 디버그 타입 출력여부
+    'newline'    => true  # 개행문자 출력여부
+]);
+
+# resource
+R::parser(_ROOT_PATH_.'/'._CONFIG_.'/components/components.json', 'components');
+
+
+// Log::d(
+//     'column name : '.ColumnsEnum::NAME->name,
+//     'column value : '.ColumnsEnum::NAME->value,
+//     'column type : '.SchemaEnum::NAME->value,
+//     'column label : '.R::components('columns')[ColumnsEnum::NAME->value]
+// );
+Log::d("==========");
+
+# 전체
+Log::d( SchemaEnum::fetchByName('name') );
+?>
