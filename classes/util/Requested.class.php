cd128b767978a99ee3b689de54bb06142a21ae09
fancyupsoft <apmsoft@gmail.com>
2023-09-26 14:38:01 +0900

    reactPHP Message ServerRequestInterface 확장용 추가

---
+++ classes/util/Requested.class.php
@@ -0,0 +1,62 @@
+<?php
+namespace Flex\Util;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+# reactphp ServerRequestInterface 용 확장 클래스
+class Requested
+{
+    private string $version = '0.5';
+	private array $params   = [];
+
+	public function __construct(
+        private ServerRequestInterface $request
+	){}
+
+	public function post() : Requested
+	{
+		if ($this->request->getHeaderLine('Content-Type') == 'application/json') {
+            $this->params = json_decode($this->request->getBody()->getContents(),true);
+        }else {
+            $this->params = $this->request->getParsedBody();
+        }
+    return $this;
+	}
+
+	public function get() : Requested
+	{
+		$this->params = $this->request->getQueryParams();
+    return $this;
+	}
+
+	public function getHeaderLine( string $headline_key) : string
+	{
+		$result = '';
+
+		if($this->request->getHeaderLine($headline_key)) {
+            $header_value = $this->request->getHeaderLine($headline_key);
+        }
+
+		return $result;
+	}
+
+    public function fetch() : array{
+		return $this->params;
+    }
+
+	public function __get($propertyName) : mixed
+	{
+		if(isset($this->params[$propertyName])){
+			return $this->params[$propertyName];
+		}
+	}
+
+	public function __set($key, $value){
+		$this->params[$key] = $value;
+	}
+
+	public function __isset($name) {
+		return isset($this->params[$name]);
+	}
+}
+?>
