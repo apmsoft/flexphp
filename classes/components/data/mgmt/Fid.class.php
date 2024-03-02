cda9e865f44828b6f84bedb117a62af1fdb9a26a
fancyupsoft <apmsoft@gmail.com>
2024-03-02 18:02:37 +0900

    Fid class v1.2 update sort up, sort down 데이터 정렬을 위한 현재+이전, 현재+다음값 자동 찾아 배열로 리턴 기능 추가

--- classes/components/data/mgmt/Fid.class.php
+++ classes/components/data/mgmt/Fid.class.php
@@ -7,7 +7,7 @@ use Flex\Annona\Log;
 # 스트링 다단 처리
 class Fid
 {
-    const __VERSION = "1.1";
+    const __VERSION = "1.2";
     /**
      * @ T : table
      * @ db : 디비 클래스 인스턴스
@@ -83,6 +83,126 @@ class Fid
     public function orderBy(?string $asc = 'ASC') : string
     {
         return sprintf("%s+0 %s", $this->column_name, $asc);
+    }
+
+    # sort down (화살표 위)
+    public function getSortDown (string $fid) : array
+    {
+        # current fid array
+        $cur_fids = [];
+        $cur_rlt = $this->db->table($this->T)
+            ->where($this->column_name, 'LIKE-R', $fid)
+            ->orderBy( $this->orderBy() )
+            ->query();
+        while($cur_row = $cur_rlt->fetch_assoc()){
+            $cur_fids[] = $cur_row;
+        }
+        // Log::d('cur_fids',$cur_fids);
+
+        # < fid array
+        $pre_fids = [];
+        $depth = $this->getDepthCount($fid);
+        $fids = explode('.',$fid);
+
+        $pre_fid = '';
+        if($depth < 1){
+            $pre_fid = ($fids[0]-1).".";
+        }else {
+            $end_2str = substr($fid,-2);
+            if($end_2str != '01'){
+                $end_fid = ((int)$end_2str - 1);
+                $pre_fid = sprintf("%s%02d",substr($fid,0,-2),$end_fid);
+            }
+        }
+
+        if($pre_fid)
+        {
+            // Log::d('pre_fid ->',$pre_fid);
+            $pre_row = $this->db->table($this->T)
+                ->select($this->column_name)
+                ->where($this->column_name, '>=', $pre_fid)
+                ->limit(1)
+                ->orderBy( $this->orderBy() )
+                ->query()->fetch_assoc();
+            if(isset($pre_row[$this->column_name]))
+            {
+                # pre_fid
+                $pre_fid = $pre_row[$this->column_name];
+                $pre_depth = $this->getDepthCount($nxt_fid);
+                // Log::d('pre_fid', $pre_fid,'pre_depth',$pre_depth);
+
+                if($depth == $nxt_depth)
+                {
+                    # query
+                    $pre_rlt = $this->db->table($this->T)
+                        ->where($this->column_name, 'LIKE-R', $pre_fid)
+                        ->orderBy( $this->orderBy() )
+                        ->query();
+                    while($pre_row = $pre_rlt->fetch_assoc()){
+                        $pre_fids[] = $pre_row;
+                    }
+                    // Log::d('pre_fids',$pre_fids);
+                }
+            }
+        }
+
+        return [
+            'cur_fids' => $cur_fids,
+            'pre_fids' => $pre_fids
+        ];
+    }
+
+    # sort up (화살표 다운)
+    public function getSortUp (string $fid) : array
+    {
+        # current fid array
+        $cur_fids = [];
+        $cur_rlt = $this->db->table($this->T)
+            ->where($this->column_name, 'LIKE-R', $fid)
+            ->orderBy( $this->orderBy() )
+            ->query();
+        while($cur_row = $cur_rlt->fetch_assoc()){
+            $cur_fids[] = $cur_row;
+        }
+        // Log::d('cur_fids',$cur_fids);
+
+        # > fid array
+        $nxt_fids = [];
+        $depth = $this->getDepthCount($fid);
+        $fids = explode('.',$fid);
+        $nxt_fid = ($depth < 1) ? ($fids[0]+1)."." : sprintf("%s%02d",substr($fid,0,-2),((int)substr($fid,-2) + 1));
+        // Log::d('nxt_fid ->',$nxt_fid);
+        $nxt_row = $this->db->table($this->T)
+            ->select($this->column_name)
+            ->where($this->column_name, '>=', $nxt_fid)
+            ->limit(1)
+            ->orderBy( $this->orderBy() )
+            ->query()->fetch_assoc();
+        if(isset($nxt_row[$this->column_name]))
+        {
+            # nxt_fid
+            $nxt_fid = $nxt_row[$this->column_name];
+            $nxt_depth = $this->getDepthCount($nxt_fid);
+            // Log::d('nxt_fid', $nxt_fid,'nxt_depth',$nxt_depth);
+
+            if($depth == $nxt_depth)
+            {
+                # query
+                $nxt_rlt = $this->db->table($this->T)
+                    ->where($this->column_name, 'LIKE-R', $nxt_fid)
+                    ->orderBy( $this->orderBy() )
+                    ->query();
+                while($nxt_row = $nxt_rlt->fetch_assoc()){
+                    $nxt_fids[] = $nxt_row;
+                }
+                // Log::d('nxt_fids',$nxt_fids);
+            }
+        }
+
+        return [
+            'cur_fids' => $cur_fids,
+            'nxt_fids' => $nxt_fids
+        ];
     }
 }
 ?>
