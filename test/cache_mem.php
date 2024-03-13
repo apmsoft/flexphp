2af71562c21ff46cd1574f35ac5e56e09593519e
fancyupsoft <apmsoft@gmail.com>
2024-03-13 10:22:37 +0900

    cache_mem test update

--- test/cache_mem.php
+++ test/cache_mem.php
@@ -14,10 +14,14 @@ Log::init(Log::MESSAGE_ECHO);
 // 예제 사용
 // CacheMem 객체 생성
 try{
-    $cacheMem = new \Flex\Annona\Cache\CacheMem('mc');
+    $cacheMem = new \Flex\Annona\Cache\CacheMem();
+
+    Log::d('CacheMem Version',CacheMem::__version);
+    Log::d('Memcache Version',$cacheMem->getVersion());
+    Log::d('Status', $cacheMem->getStats());
 
     $data = ["a","b","c"];
-    $cache_data = $cacheMem->getCache() ?? $cacheMem->setCache($data, 60)->getCache();
+    $cache_data = $cacheMem('mc')->_get() ?? $cacheMem->_set($data, 60)->_get();
     Log::d($cache_data);
 }catch(\Exception $e){
     Log::e($e->getMessage());
