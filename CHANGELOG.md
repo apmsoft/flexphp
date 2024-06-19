294bd8993ea5f68defd25f900ce766d7ee97994a
fancyupsoft <apmsoft@gmail.com>
2024-06-19 11:30:41 +0900

    - out 패키지에 JsonTransformer 클래스 추가 jsonString 으로 변환하되 numberic 타입은 numberic 타입으로 자동 형변화 기능

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Changelog
 
 ## [1.2]
+### - 2024-06-19
+- out 패키지에 JsonTransformer 클래스 추가 jsonString 으로 변환하되 numberic 타입은 numberic 타입으로 자동 형변화 기능
 
 ### - 2024-05-09
 - HttpRequest v1.0.2 -> v1.0.3, set 함수 추가 (생성자에서 배열로 등록해야만 했던 요청 주소및파라메터들을 쉽게 등록할 수 있도록 업데이트)
---
+++ classes/annona/out/JsonTransformer.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace Flex\Annona\Out;
+
+final class JsonTransformer
+{
+	public const __version = '0.1.1';
+
+	# 배열을 json string utf8
+	final public static function toJson(array $data, array $except_numberic_keys = [], int $options=JSON_UNESCAPED_UNICODE) : string
+	{
+		$data = self::applyNumericExceptions($data, $except_numberic_keys);
+        return json_encode($data, $options);
+
+	}
+
+	# 특정키를 제외한 numberic으로 변형하기
+	private static function applyNumericExceptions(array $data, array $exceptNumericKeys) : array
+    {
+        foreach ($data as $key => &$value) {
+            if (is_array($value)) {
+                $value = self::applyNumericExceptions($value, $exceptNumericKeys);
+            } elseif (is_numeric($value) && in_array($key, $exceptNumericKeys)) {
+                $value = (string) $value;
+            } elseif (is_numeric($value)) {
+                $value = $value + 0; // 숫자 형 변환
+            }
+        }
+        return $data;
+    }
+
+}
+?>
